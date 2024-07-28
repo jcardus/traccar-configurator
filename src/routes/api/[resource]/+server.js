e7ff20d51a0fff3c2840e705d507c53f68ffad72
jcardus <joaquim.cardeira@gmail.com>
2024-07-28 03:43:08 +0100

    forward to traccar

--- src/routes/api/[resource]/+server.js
+++ src/routes/api/[resource]/+server.js
@@ -1,5 +1,7 @@
 export async function GET({ request, platform }) {
+    const host = platform.env.TRACCAR_SERVER || import.meta.env.VITE_TRACCAR_SERVER
     const url = new URL(request.url.replace('https://', 'http://'))
-    url.host = platform.env.TRACCAR_SERVER
+    url.host = host
+    url.port = 80
     return fetch(new Request(url, request))
 }
