adf7556b50ec89b459c5ea34a6ac5cca44060945
jcardus <joaquim.cardeira@gmail.com>
2024-07-28 07:07:53 +0100

    go to login when there's no session

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -2,7 +2,17 @@
     import '../../app.css';
     import Navbar from './Navbar.svelte';
     import Sidebar from './Sidebar.svelte';
+    import {session} from "$lib/store.js";
+    import {goto} from "$app/navigation";
+    import {onMount} from "svelte";
     let drawerHidden = false;
+
+    onMount(() => {
+        let email = $session.email;
+        if (!email) {
+            goto('/login');
+        }
+    })
 </script>
 
 <header class="fixed top-0 z-40 mx-auto w-full flex-none border-b border-gray-200 bg-white dark:border-gray-600 dark:bg-gray-800">
