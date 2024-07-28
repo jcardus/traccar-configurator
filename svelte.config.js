a86f25ada3ca65579afb90f1ced54f1012e85f98
jcardus <joaquim.cardeira@gmail.com>
2024-07-28 05:09:26 +0100

    add login page import vitePreprocess to enable processing <style> blocks as PostCSS. override theme

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,14 @@
-import adapter from '@sveltejs/adapter-cloudflare';
-
-/** @type {import('@sveltejs/kit').Config} */
+//import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-auto';
+import {vitePreprocess} from "@sveltejs/vite-plugin-svelte";
 const config = {
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
-	}
+	},
+	preprocess: vitePreprocess()
 };
 
 export default config;
