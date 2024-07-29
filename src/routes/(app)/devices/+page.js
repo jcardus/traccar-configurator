474e931a0975c6204678da8e873eecc3d8b5dae5
jcardus <asklocation.net@gmail.com>
2024-07-29 04:09:27 +0100

    fix fetch devices

--- src/routes/(app)/devices/+page.js
+++ src/routes/(app)/devices/+page.js
@@ -3,11 +3,16 @@ import {setError} from "$lib/store.js";
 export const ssr = false
 
 export async function load() {
-    const response = await fetch('/api/devices');
-    const devices = await response.json()
-    if (response.ok) {
-        return {devices: devices.slice(0, 500)}
-    } else {
-        setError(await response.text())
+    try {
+        const response = await fetch('/api/devices');
+        const devices = await response.json()
+        if (response.ok) {
+            return {devices: devices.slice(0, 500)}
+        } else {
+            setError(await response.text())
+        }
+    } catch (e) {
+        setError(e.message)
     }
+    return {devices: []}
 }
