363c5cb53695172bcd0fccc47056f17d47c4f72c
jcardus <asklocation.net@gmail.com>
2024-07-29 04:17:02 +0100

    fix devices error again

--- src/routes/(app)/devices/+page.js
+++ src/routes/(app)/devices/+page.js
@@ -5,8 +5,8 @@ export const ssr = false
 export async function load() {
     try {
         const response = await fetch('/api/devices');
-        const devices = await response.json()
         if (response.ok) {
+            const devices = await response.json()
             return {devices: devices.slice(0, 500)}
         } else {
             setError(await response.text())
