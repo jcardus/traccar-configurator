2edb81bd294a5853e640b06f45134e3ff3001103
jcardus <asklocation.net@gmail.com>
2024-08-26 03:31:10 +0100

    fiz session ignore .wrangler min-h-full body 100vh

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -18,9 +18,9 @@
 <header class="fixed top-0 z-40 mx-auto w-full flex-none border-b border-gray-200 bg-white dark:border-gray-600 dark:bg-gray-800">
     <Navbar bind:drawerHidden />
 </header>
-<div class="overflow-hidden lg:flex">
+<div class="flex min-h-full">
     <Sidebar bind:drawerHidden />
-    <div class="relative h-full w-full overflow-y-auto lg:ml-64 pt-[60px]">
+    <div class="min-h-full grow overflow-y-auto pt-[60px]">
         <slot />
     </div>
 </div>
