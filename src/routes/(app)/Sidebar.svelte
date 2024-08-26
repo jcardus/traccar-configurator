10d2eb7055db9e56ec7ce142e4478c3bfb52d6e4
jcardus <asklocation.net@gmail.com>
2024-08-26 04:13:04 +0100

    w-auto lg:w-64

--- src/routes/(app)/Sidebar.svelte
+++ src/routes/(app)/Sidebar.svelte
@@ -44,9 +44,8 @@
         class={drawerHidden ? 'hidden' : ''}
         activeUrl={mainSidebarUrl}
         activeClass="bg-gray-100 dark:bg-gray-700"
-        asideClass="fixed sm:static inset-0 z-30 min-h-full w-64 lg:h-auto border-e border-gray-200 dark:border-gray-600 lg:overflow-y-visible lg:pt-16 lg:block"
+        asideClass="fixed sm:static inset-0 z-30 min-h-full w-auto lg:w-64 lg:h-auto border-e border-gray-200 dark:border-gray-600 lg:overflow-y-visible lg:pt-16 lg:block"
 >
-    <h4 class="sr-only">Main menu</h4>
     <SidebarWrapper
             divClass="overflow-y-auto px-3 pt-20 lg:pt-5 h-full bg-white scrolling-touch max-w-2xs lg:h-[calc(100vh-4rem)] lg:block dark:bg-gray-800 lg:me-0 lg:sticky top-2"
     >
@@ -85,10 +84,3 @@
         </nav>
     </SidebarWrapper>
 </Sidebar>
-<div
-        hidden={drawerHidden}
-        class="fixed inset-0 z-20 bg-gray-900/50 dark:bg-gray-900/60"
-        on:click={closeDrawer}
-        on:keydown={closeDrawer}
-        role="presentation"
-/>
