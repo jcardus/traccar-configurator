12707b891a43de6951faa1abf85b1e6599df1226
entrack-plataforma <entrack.plataforma@gmail.com>
2024-09-08 15:16:22 +0100

    teltonika flush

--- src/lib/commands.js
+++ src/lib/commands.js
@@ -1,7 +1,12 @@
+const host = 'gps.rastreosat.com.br'
 export const getData = (device, deviceType, apn='allcom.vivo.com.br') => ({
-    Teltonika: () => [`  setparam 2001:${apn}`, `  setparam 2004:gps.rastreosat.com.br;2005:5027`],
+    Teltonika: ({uniqueId}) => [
+        `  setparam 2001:${apn}`,
+        `  setparam 2004:${host};2005:5027`,
+        `  flush ${uniqueId},${apn},,,${host},5027,0`,
+    ],
     Aovx: () => ['config aovx'],
-    Suntech: ({model, uniqueId}) => [`${model};${uniqueId};02;0;${apn};allcom;allcom;gps.rastreosat.com.br;5011;;;;`],
+    Suntech: ({model, uniqueId}) => [`${model};${uniqueId};02;0;${apn};allcom;allcom;${host};5011;;;;`],
 }[deviceType](device))
 
 export const deviceTypes = ['Suntech', 'Aovx', 'Teltonika'].sort().map(d => ({name: d, value: d}))
