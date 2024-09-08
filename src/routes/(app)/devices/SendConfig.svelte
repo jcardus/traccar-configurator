ce1db236c79bb0fd949979d3a9afec4b2882ece3
entrack-plataforma <entrack.plataforma@gmail.com>
2024-09-08 01:21:47 +0100

    select config type

--- src/routes/(app)/devices/SendConfig.svelte
+++ src/routes/(app)/devices/SendConfig.svelte
@@ -1,21 +1,24 @@
 <script>
 import {setAlert} from "$lib/store.js";
-import {Button, Modal} from "flowbite-svelte";
-import {QuestionCircleOutline} from "flowbite-svelte-icons";
+import {Button, Modal, Select} from "flowbite-svelte";
 export let open = false
 export let selected = [];
+const deviceTypes = ['Suntech', 'Aovx'].sort().map(d => ({name: d, value: d}))
+let deviceType = 'Aovx'
 </script>
 
-<Modal bind:open size="sm">
-    <QuestionCircleOutline class="mx-auto mb-4 mt-8 h-10 w-10 text-red-600" />
-    <h3 class="mb-6 text-center text-lg text-gray-500 dark:text-gray-400">
-        Send configuration to {selected.length} devices?
-    </h3>
+<Modal bind:open>
     <div class="flex items-center justify-center">
+        Send
+    <Select items={deviceTypes} bind:value={deviceType} class="w-30 m-4" />
+    configuration to {selected.length} devices?
+    </div>
+    <div class="flex items-center justify-center">
+
         <Button color="red" class="mr-2" on:click={() => {
             setAlert('Configuration sent.')
             open = false
         }}>Yes</Button>
-        <Button color="alternative" on:click={() => (open = false)}>No, cancel</Button>
+        <Button  color="alternative" on:click={() => (open = false)}>No, cancel</Button>
     </div>
 </Modal>
