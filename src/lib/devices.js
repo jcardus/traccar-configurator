19c65ceb56c02d097ef156fda4d2ca77401a75f5
entrack-plataforma <entrack.plataforma@gmail.com>
2024-09-08 20:39:04 +0100

    fix teltonika priority

--- src/lib/devices.js
+++ src/lib/devices.js
@@ -6,21 +6,37 @@ const concox =  ({}, apn) => [
     `APN,${apn}#`
 ]
 
-export const getData = (device, apn='allcom.vivo.com.br') => ({
+const teltonika = ({uniqueId}, apn) => [
+    `  setparam 2001:${apn}`,
+    `  setparam 2004:${host};2005:5027;50190:2;50191:5`,
+    `  flush ${uniqueId},${apn},,,${host},5027,0`,
+]
+
+const messages = {
     concox,
-    teltonika: ({uniqueId}) => [
-        `  setparam 2001:${apn}`,
-        `  setparam 2004:${host};2005:5027`,
-        `  flush ${uniqueId},${apn},,,${host},5027,0`,
-    ],
-    aovx: () => ['config aovx'],
+    teltonika,
+    fmb920: teltonika,
+    gl100: () => ['config aovx'],
     ST300CMD: ({model, uniqueId}) => [`${model};${uniqueId};02;0;${apn};allcom;allcom;${host};5011;;;;`],
-}[device.model](device, apn))
+}
+
+export const getData = (device, apn='allcom.vivo.com.br') => {
+    if (!messages[device.model]) {
+        alert('invalid device model ' + device.model);
+        return
+    }
+    return messages[device.model](device, apn)
+}
+
+/*?
+const teltonikaDevices = () => {
+    for (const model of ['001', '120', '130', '140', '920'])
+}*/
 
 export const deviceTypes = [
         {name: 'Concox', value: 'concox'},
         {name: 'Suntech ST310u', value:'ST300CMD'},
-        {name: 'Aovx GL100', value: 'aovx'},
-        {name: 'Aovx VL100', value: 'aovx'},
-        {name: 'Teltonika', value: 'teltonika'}
+        {name: 'Aovx GL100', value: 'gl100'},
+        {name: 'Aovx VL100', value: 'vl100'},
+        {name: 'Teltonika FMB920', value: 'fmb920'},
     ].sort((a, b) => a.name.localeCompare(b.name))
