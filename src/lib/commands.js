47479f30b8b1b4f4fe2f6eb6b699b046cfb45c33
entrack-plataforma <entrack.plataforma@gmail.com>
2024-09-08 14:23:05 +0100

    setparam 2

--- src/lib/commands.js
+++ src/lib/commands.js
@@ -1,5 +1,5 @@
 export const getData = (device, deviceType, apn='allcom.vivo.com.br') => ({
-    Teltonika: () => [`  setparam 2001:${apn};2004:gps.rastreosat.com.br;2005:5027`],
+    Teltonika: () => [`  setparam 2001:${apn}`, `  setparam 2004:gps.rastreosat.com.br;2005:5027`],
     Aovx: () => ['config aovx'],
     Suntech: ({model, uniqueId}) => [`${model};${uniqueId};02;0;${apn};allcom;allcom;gps.rastreosat.com.br;5011;;;;`],
 }[deviceType](device))
