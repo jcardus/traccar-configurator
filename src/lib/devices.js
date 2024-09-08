3dea4e18aa98820ab7adedf8176e88699924fe86
entrack-plataforma <entrack.plataforma@gmail.com>
2024-09-08 17:09:18 +0100

    use device model for sms

--- src/lib/devices.js
+++ src/lib/devices.js
@@ -1,21 +1,21 @@
 const host = 'gps.rastreosat.com.br'
 
-const Concox =  ({}, apn) => [
+const concox =  ({}, apn) => [
     `SERVER,0,${host},5023,0#`,
     `SERVER,1,${host},5023,0#`,
     `APN,${apn}#`
 ]
 
-export const getData = (device, deviceType, apn='allcom.vivo.com.br') => ({
-    Concox,
-    Teltonika: ({uniqueId}) => [
+export const getData = (device, apn='allcom.vivo.com.br') => ({
+    concox,
+    teltonika: ({uniqueId}) => [
         `  setparam 2001:${apn}`,
         `  setparam 2004:${host};2005:5027`,
         `  flush ${uniqueId},${apn},,,${host},5027,0`,
     ],
-    Aovx: () => ['config aovx'],
-    Suntech: ({model, uniqueId}) => [`${model};${uniqueId};02;0;${apn};allcom;allcom;${host};5011;;;;`],
-}[deviceType](device, apn))
+    aovx: () => ['config aovx'],
+    ST300CMD: ({model, uniqueId}) => [`${model};${uniqueId};02;0;${apn};allcom;allcom;${host};5011;;;;`],
+}[device.model](device, apn))
 
 export const deviceTypes = [
         {name: 'Concox', value: 'concox'},
--- src/routes/(app)/devices/SendConfig.svelte
+++ src/routes/(app)/devices/SendConfig.svelte
@@ -1,24 +1,28 @@
 <script>
-    import {Button, Input, Label, Modal, Select} from "flowbite-svelte";
+    import {Button, Input, Label, Modal} from "flowbite-svelte";
 import {sendCommand} from "$lib";
-import {setError} from "$lib/store.js";
-import {getData, deviceTypes} from "$lib/devices.js";
+import {setError, setAlert} from "$lib/store.js";
+import {getData} from "$lib/devices.js";
 export let open = false
 export let selected = [];
 export let devices = [];
-let deviceType = 'Aovx'
 let apn
 
 async function sendConfiguration() {
     try {
         for (const deviceId of selected) {
-            for (const data of getData(devices.find(device => device.id === deviceId), deviceType, apn)) {
-                await sendCommand({
-                    type: 'custom',
-                    attributes: {data},
-                    textChannel: true,
-                    deviceId
-                })
+            const device = devices.find(device => device.id === deviceId)
+            if (!device.model) {
+                setAlert('Please select model for ' + device.name)
+            } else {
+                for (const data of getData(device, apn)) {
+                    await sendCommand({
+                        type: 'custom',
+                        attributes: {data},
+                        textChannel: true,
+                        deviceId
+                    })
+                }
             }
         }
     } catch (e) {
@@ -31,10 +35,6 @@ async function sendConfiguration() {
 <Modal bind:open title="Send configuration to {selected.length} devices">
     <div class="space-y-6 p-0">
     <div class="grid grid-cols-6 gap-6">
-        <Label class="col-span-6 space-y-2 sm:col-span-3">Manufacturer
-        <Select items={deviceTypes} bind:value={deviceType} class="mt-2"/>
-        </Label>
-
         <Label class="col-span-6 space-y-2 sm:col-span-3">APN
         <Input bind:value={apn}  class="mt-2" placeholder="allcom.vivo.com.br" required />
         </Label>
