19e61d5d786984704cd27d5ad1cb5b17c7347397
entrack-plataforma <entrack.plataforma@gmail.com>
2024-09-15 18:51:27 +0100

    configure pwa add pwa-assets-generator

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
     import { error, clearError, alert, clearAlert } from '$lib/store';
     import { onMount } from 'svelte';
     import {Alert} from "flowbite-svelte";
+    import { pwaInfo } from 'virtual:pwa-info';
+    import { pwaAssetsHead } from 'virtual:pwa-assets/head';
+
+    $: webManifestLink = pwaInfo ? pwaInfo.webManifest.linkTag : ''
 
     let errorMessage = '';
     let alertMessage = '';
@@ -34,9 +38,18 @@
 
 </script>
 <slot/>
+
 <svelte:head>
     <title>Configurator {appVersion}</title>
+    {#if pwaAssetsHead.themeColor}
+        <meta name="theme-color" content={pwaAssetsHead.themeColor.content} />
+    {/if}
+    {#each pwaAssetsHead.links as link}
+        <link {...link} />
+    {/each}
+    {@html webManifestLink}
 </svelte:head>
+
 {#if errorMessage}
     <div class="fixed top-4 left-1/2 transform -translate-x-1/2 z-50">
     <Alert>
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,31 @@ import { SvelteKitPWA } from '@vite-pwa/sveltekit'
 export default defineConfig({
 	plugins: [
 		sveltekit(),
-		SvelteKitPWA()
+		SvelteKitPWA({
+			manifest: {
+				short_name: 'Config',
+				name: 'Configurator',
+				theme_color: '#A5371B',
+				icons: [
+					{
+						src: 'pwa-64x64.png',
+						sizes: '64x64',
+						type: 'image/png',
+					},
+					{
+						src: 'pwa-192x192.png',
+						sizes: '192x192',
+						type: 'image/png',
+					},
+					{
+						src: 'pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'any maskable',
+					},
+				]
+			}
+		})
 	],
 	server: {
 		proxy: {
