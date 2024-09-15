515a9144353b885b8751e84c7428e75f5805dd4f
entrack-plataforma <entrack.plataforma@gmail.com>
2024-09-15 23:42:36 +0100

    loading in send config

--- src/routes/(app)/devices/SendConfig.svelte
+++ src/routes/(app)/devices/SendConfig.svelte
@@ -1,15 +1,16 @@
 <script>
-    import {Button, Modal} from "flowbite-svelte";
+    import {Button, Modal, Spinner} from "flowbite-svelte";
 import {sendCommand} from "$lib";
 import {setError, setAlert} from "$lib/store.js";
 import {getData} from "$lib/devices.js";
 export let open = false
 export let selected = [];
 export let devices = [];
-let apn
+let apn, sending=false
 
 async function sendConfiguration() {
     try {
+        sending = true
         for (const deviceId of selected) {
             const device = devices.find(device => device.id === deviceId)
             if (!device.model) {
@@ -29,13 +30,16 @@ async function sendConfiguration() {
         setError(e)
     }
     open = false
+    sending = false
 }
 </script>
 
 <Modal bind:open title="Send configuration" class="w-96">
     <div>Send to {selected.length} device{selected.length>1?'s':''}?</div>
     <div class="flex items-center justify-center">
-        <Button color="red" class="mr-2" on:click={sendConfiguration}>Yes</Button>
-        <Button color="alternative" on:click={() => (open = false)}>No, cancel</Button>
+        <Button color="red" class="m-2" on:click={sendConfiguration} disabled="{sending}">
+            <Spinner class="{sending || 'hidden'}" size="{4}"></Spinner>
+            <span class="pl-1">{sending?'Sending...':'Yes'}</span></Button>
+        <Button color="alternative" on:click={() => open = false}>No, cancel</Button>
     </div>
 </Modal>
