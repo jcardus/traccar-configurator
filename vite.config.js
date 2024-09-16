dd962cf81ed4abf1aacf41edceb7e7a9e9684c1b
entrack-plataforma <entrack.plataforma@gmail.com>
2024-09-16 20:41:39 +0100

    add sentry add proxy in dev

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,44 @@
+import { sentrySvelteKit } from "@sentry/sveltekit";
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { SvelteKitPWA } from '@vite-pwa/sveltekit'
 
 export default defineConfig({
-	plugins: [
-		sveltekit(),
-		SvelteKitPWA({
-			manifest: {
-				short_name: 'Config',
-				name: 'Configurator',
-				theme_color: '#A5371B',
-				icons: [
-					{
-						src: 'pwa-64x64.png',
-						sizes: '64x64',
-						type: 'image/png',
-					},
-					{
-						src: 'pwa-192x192.png',
-						sizes: '192x192',
-						type: 'image/png',
-					},
-					{
-						src: 'pwa-512x512.png',
-						sizes: '512x512',
-						type: 'image/png',
-						purpose: 'any maskable',
-					},
-				]
-			}
-		})
-	]
+	plugins: [sentrySvelteKit({
+        sourceMapsUploadOptions: {
+            org: "rastreosat-jo",
+            project: "javascript-sveltekit",
+            url: "https://sentry.io/"
+        }
+    }), sveltekit(), SvelteKitPWA({
+        manifest: {
+            short_name: 'Config',
+            name: 'Configurator',
+            theme_color: '#A5371B',
+            icons: [
+                {
+                    src: 'pwa-64x64.png',
+                    sizes: '64x64',
+                    type: 'image/png',
+                },
+                {
+                    src: 'pwa-192x192.png',
+                    sizes: '192x192',
+                    type: 'image/png',
+                },
+                {
+                    src: 'pwa-512x512.png',
+                    sizes: '512x512',
+                    type: 'image/png',
+                    purpose: 'any maskable',
+                },
+            ]
+        }
+    })],
+	server: {
+		proxy: {
+			'/api/socket': 'ws://' + process.env.VITE_TRACCAR_SERVER,
+			'/api': 'http://' + process.env.VITE_TRACCAR_SERVER
+		}
+	}
 });
