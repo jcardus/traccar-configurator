fae3c166aa86120a5c26e46093bd6ce8bde3419a
jcardus <asklocation.net@gmail.com>
2024-09-30 23:50:51 +0100

    simplify navbar

--- src/routes/(app)/Navbar.svelte
+++ src/routes/(app)/Navbar.svelte
@@ -10,7 +10,6 @@
     import NavContainer from "flowbite-svelte/NavContainer.svelte";
 
     export let drawerHidden=false;
-    export let fluid=true
 </script>
 <Navbar>
     <NavContainer>
@@ -18,21 +17,14 @@
                 onClick={() => (drawerHidden = !drawerHidden)}
                 class="m-0 me-3 md:block lg:hidden"
         />
-        <NavBrand href="/" class="lg:w-60">
-            <img
-                    src="/logo.svg"
-                    class="me-2.5 h-6 sm:h-8"
-                    alt="Logo"
-            />
-            <span
-                    class="ml-px self-center whitespace-nowrap text-xl font-semibold dark:text-white sm:text-2xl"
-            >
-				Configurator
-			</span>
-            <div class="ms-auto flex items-center text-gray-500 dark:text-gray-400 sm:order-2">
-                <DarkMode />
-                <UserMenu />
-            </div>
+        <NavBrand href="/">
+            <img src="/logo.svg" class="me-2.5 h-6 sm:h-8" alt="Logo"/>
+            <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span>
+
         </NavBrand>
+        <div class="ms-auto flex items-center text-gray-500 dark:text-gray-400 sm:order-2">
+            <DarkMode />
+            <UserMenu />
+        </div>
     </NavContainer>
 </Navbar>
