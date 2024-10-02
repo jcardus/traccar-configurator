5e4724700161c2212a77afc530280c0be3dab0ce
jcardus <joaquim_cardeira@hotmail.com>
2024-10-02 01:17:07 +0100

    remove navcontainer

--- src/routes/(app)/Navbar.svelte
+++ src/routes/(app)/Navbar.svelte
@@ -7,24 +7,21 @@
         DarkMode
     } from 'flowbite-svelte';
     import UserMenu from "./UserMenu.svelte";
-    import NavContainer from "flowbite-svelte/NavContainer.svelte";
 
     export let drawerHidden=false;
 </script>
 <Navbar>
-    <NavContainer>
-        <NavHamburger
-                onClick={() => (drawerHidden = !drawerHidden)}
-                class="m-0 me-3 md:block lg:hidden"
-        />
-        <NavBrand href="/">
-            <img src="/logo.svg" class="me-2.5 h-6 sm:h-8" alt="Logo"/>
-            <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span>
+    <NavHamburger
+            onClick={() => (drawerHidden = !drawerHidden)}
+            class="m-0 me-3 md:block lg:hidden"
+    />
+    <NavBrand href="/">
+        <img src="/logo.svg" class="me-2.5 h-6 sm:h-8" alt="Logo"/>
+        <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Configurator</span>
 
-        </NavBrand>
-        <div class="ms-auto flex items-center text-gray-500 dark:text-gray-400 sm:order-2">
-            <DarkMode />
-            <UserMenu />
-        </div>
-    </NavContainer>
+    </NavBrand>
+    <div class="ms-auto flex items-center text-gray-500 dark:text-gray-400 sm:order-2">
+        <DarkMode />
+        <UserMenu />
+    </div>
 </Navbar>
