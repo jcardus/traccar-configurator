fbfb67d10f9b01648839c18dd3d0d35dad9f8ae7
joaquimcardeira <joaquim.cardeira@gmail.com>
2026-03-28 20:32:44 +0000

    sms

--- src/routes/(app)/SendConfig.svelte
+++ src/routes/(app)/SendConfig.svelte
@@ -1,12 +1,12 @@
 <script>
-    import {Button, Modal, Spinner} from "flowbite-svelte";
+    import {Button, Modal, Spinner, Toggle} from "flowbite-svelte";
 import {sendCommand} from "$lib";
 import {setError, setAlert} from "$lib/store.js";
 import {getData} from "$lib/devices.js";
 export let open = false
 export let selected = [];
 export let devices = [];
-let apn, sending=false
+let apn, sending=false, sms=true
 
 async function sendConfiguration() {
     try {
@@ -29,7 +29,7 @@ async function sendConfiguration() {
                 await sendCommand({
                     type: 'custom',
                     attributes: {data},
-                    textChannel: true,
+                    textChannel: sms,
                     deviceId
                 })
             }
@@ -46,6 +46,9 @@ async function sendConfiguration() {
 
 <Modal bind:open title="Send configuration" class="w-96">
     <div>Send to {selected.length} device{selected.length>1?'s':''}?</div>
+    <div class="flex items-center mt-2">
+        <Toggle bind:checked={sms}>Send via SMS</Toggle>
+    </div>
     <div class="flex items-center justify-center">
         <Button color="red" class="m-2" onclick={sendConfiguration} disabled="{sending}">
             <Spinner class="{sending || 'hidden'}" size="{4}"></Spinner>
